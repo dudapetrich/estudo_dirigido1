16f3d7905c21c3d811abe92cbb07283c9b3dc2fd
dudapetrich <48135709+dudapetrich@users.noreply.github.com>
2019-03-19 11:07:45 -0300

    Add files via upload

---
+++ contador.php
@@ -0,0 +1,10 @@
+<?php
+//o que acontece ao se executar o algoritmo abaixo?
+
+$contador=0;
+ while ($contador<=3){
+     print $contador;
+     $contador++; #soma mais um a vertical
+ }
+
+ print"\n saiu da repetição";
